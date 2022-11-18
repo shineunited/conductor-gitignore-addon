06ab0d243df01e611d36cb559f8922c99b47834a
Robert Lasseter <rlasseter@shineunited.com>
2022-11-18 11:46:08 -0600

    refactor generators to use interfaces

---
+++ src/Blueprint/GitignoreBlueprintInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of Conductor Gitignore Addon.
+ *
+ * (c) Shine United LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ShineUnited\Conductor\Addon\Gitignore\Blueprint;
+
+use ShineUnited\Conductor\Filesystem\Blueprint\BlueprintInterface;
+
+/**
+ * Gitignore Blueprint Interface
+ */
+interface GitignoreBlueprintInterface extends BlueprintInterface {
+
+}
